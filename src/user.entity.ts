75ef64446bdc389a9cfefa15837be6cf22fe6809
zidni-bwi <23148293+zidni-bwi@users.noreply.github.com>
2022-03-04 06:48:22 +0800

    Add files via upload

---
+++ src/user.entity.ts
@@ -0,0 +1,8 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn() id: number;
+  @Column({unique: true}) username: string;
+  @Column() password: string;
+}
